b2f3be71d4cc04bdff99b2f01d2e5ab29257bf49
Dominic Meyer <dominic.meyer@me.com>
2020-01-18 17:36:24 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -158,7 +158,6 @@ startTimer();
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
-
 document.getElementById('load-quote').addEventListener("click", function() {
   printQuote();
   startTimer();
